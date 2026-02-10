5043e7d3d420c9148d998e5bd87a0f91ad5396d7
jbailey903 <jbailey903@boyslatin.net>
2026-02-10 15:25:03 +0000

    first commit

---
+++ springreview.py
@@ -0,0 +1,21 @@
+#take a price and depending on the price amount the user will get
+# a 15$, 25$ discount or no discount
+
+# input- we need to take in the item price as an input 
+# the output needs to calculate and show the  discount amount 
+# needs to be a function 
+# if price is > 50 but < 75
+# if price > 75 = should be 25% off
+# conditional statement if / else 
+
+def discount{}:
+    price = input (' how much is the item')
+    if price > 50 and < 75:
+        discount=.15
+        sum= price .15
+        total= price- sum
+        print(" your new total is + total")
+        else:
+            print('sorry, no discount')
+
+            discount():
