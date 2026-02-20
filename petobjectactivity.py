8d0ec5d9362f029f19e26969acc47ad8a0a6b309
jbailey903 <jbailey903@boyslatin.net>
2026-02-20 15:26:22 +0000

    pets

---
+++ petobjectactivity.py
@@ -0,0 +1,10 @@
+class pet_construuctor:
+def __init__(self, name,type,age,color):
+self.name=Name
+self.type=type
+self.age=age
+self.color=color
+
+pet_1= pet_constructor("Brutus " , "Dog", " black")
+pet_2= pet_constructor('slime',' snake','white')
+pet_3= pet_constructor(' mittens', 'cat'. ' black and gray')
