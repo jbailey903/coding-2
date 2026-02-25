4cde103a561dac12226e178a66dbcf3b226c3ab6
jbailey903 <jbailey903@boyslatin.net>
2026-02-25 15:24:27 +0000

    banking

---
+++ bankingstsyem.py
@@ -0,0 +1,10 @@
+class JaquanFirstBank:
+    def __int __(self):
+     self.name=Name
+    self.age=age
+    self.balance=balnce    
+self.accountnumber= accountnumber 
+self.phone=phone
+
+def deposit(self,balance,amount):
+   balance+= amount
