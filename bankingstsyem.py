ac4aebcac1dd8e4298aef9e73ec41f629c69d2e2
jbailey903 <jbailey903@boyslatin.net>
2026-02-27 15:26:37 +0000

    hhhh

--- bankingstsyem.py
+++ bankingstsyem.py
@@ -8,3 +8,17 @@ self.phone=phone
 
 def deposit(self,balance,amount):
    balance+= amount
+
+
+   def createaccount():
+      print(" welcome to Jaquans First Bank" \
+      (" please complete the form to create an account")
+      name=input(" please type in your name")
+      age=int (input)(
+         accountnumber = random,randrange(1,10)
+
+   print("congrats! your account is complete") 
+
+   account= JaquanFirstBank(name, age,balance, accountnumber,'na') 
+
+   return 
