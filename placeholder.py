535f4ea4a458a215714e066b0b7a09281d00bd49
jbailey903 <jbailey903@boyslatin.net>
2026-04-09 14:26:30 +0000

    ooooo

---
+++ placeholder.py
@@ -0,0 +1,14 @@
+Import requests
+
+url = 'https://jsonplaceholder.typicode.com/posts/1'
+
+response = requests.get (url)
+
+print(response.json())
+
+import requests
+
+query=  https://bored-api.appbrewery.com/filter?type=education
+response= requests.get(query)
+
+print(respon
