0a7ad86d8916a14cd52259cc31fc90681e0a63f9
jbailey903 <jbailey903@boyslatin.net>
2026-04-29 14:26:52 +0000

    database

---
+++ sql_intro.py
@@ -0,0 +1,26 @@
+import_sqllite3
+
+
+
+correct=_ sqllite3. connect
+
+# creat a object that will be sent to the new data Base
+cursor= connect.cursor()
+# we nned to create a scheme (structure) for our data
+cursor.structure
+
+
+CREATE TABLE gamesales
+developer TEXT
+price INTERGER
+genre TEXT
+Sales INTERGER 
+totalsales INTERGERS
+)
+''')
+
+#5 once we can now create our first database
+cursor. execute('''
+
+connect.comit()
+conncect.close()
---
+++ studentapi.py
@@ -0,0 +1,6 @@
+
+import request 
+print(response)
+'Duck With Muscles': data('Duck With Muscles')
+query=   https://rickandmortyapi.com/api/character/109
+ response= requests.get(query)
